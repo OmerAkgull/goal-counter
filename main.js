1c219caa77dc59a7b511bb670396a8079da20b5b
OmerAkgull <143122832+OmerAkgull@users.noreply.github.com>
2023-11-08 13:54:54 +0300

    Create controls for setting the score and team names

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let counterOne = 0;
 let counterTwo = 0;
+let teamOne = "Team One";
+let teamTwo = "Team Two";
 
 function myFirstFunction() {
   counterOne++;
@@ -19,4 +21,28 @@ function myThirdFunction() {
 function myFourthFunction() {
   counterTwo--;
   document.getElementById("counterTwo").innerHTML = counterTwo;
+}
+
+function myFifthFunction() {
+  document.getElementById("counterOne").innerHTML = prompt(
+    "How many goals has your team scored?"
+  );
+}
+
+function mySixthFunction() {
+  document.getElementById("counterTwo").innerHTML = prompt(
+    "How many goals has your team scored?"
+  );
+}
+
+function mySeventhFunction() {
+  document.getElementById("teamOne").innerHTML = prompt(
+    "What is the name of your team?"
+  );
+}
+
+function myEighthFunction() {
+  document.getElementById("teamTwo").innerHTML = prompt(
+    "What is the name of your team?"
+  );
 }
